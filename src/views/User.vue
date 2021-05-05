3206675243cef9e0eb4e1dd7ca1911fd86dcac40
DriftKingTW <driftkingtw@gmail.com>
2021-05-05 23:03:21 +0800

    Fix duplicate data

--- src/views/User.vue
+++ src/views/User.vue
@@ -24,7 +24,7 @@ export default {
         .get("https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8")
         .then((data) => {
           console.log(data.data);
-          this.users = [...this.users, ...data.data];
+          this.users = [...data.data];
         });
     },
   },
