ccb728703e67f2b222b8e97261a9b1e5abe131cb
DriftKingTW <driftkingtw@gmail.com>
2021-05-05 23:32:11 +0800

    Add user card style

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,12 @@
 </template>
 
 <style lang="scss">
+@import "@/assets/master.scss";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  min-height: 100vh;
+  color: $primary;
 }
 
 #nav {
@@ -22,10 +22,10 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $secondary;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: $secondary-dark;
     }
   }
 }
--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,8 +1,9 @@
 <template>
-  <div v-if="users">
-    <div v-for="(user, index) in users" :key="index">
-      <p>Full Name: {{ user.name }}</p>
-      <p>City: {{ user.city }}</p>
+  <div class="user-list" v-if="users">
+    <div class="user-card" v-for="(user, index) in users" :key="index">
+      <img src="@/assets/default-profile.png" alt="" />
+      <p class="text-name">{{ user.name }}</p>
+      <p class="text-location">{{ user.city }}</p>
     </div>
   </div>
 </template>
@@ -15,3 +16,49 @@ export default {
   },
 };
 </script>
+
+<style scoped lang="scss">
+@import "@/assets/master.scss";
+
+.user-list {
+  display: flex;
+  justify-content: center;
+  gap: 1.5rem;
+  padding: 0.5rem;
+
+  img {
+    width: 100%;
+  }
+}
+
+.user-card {
+  min-width: 20ch;
+  max-width: 25ch;
+  box-shadow: 0 4px 8px 0 $light-gray;
+  transition: 0.3s;
+  border-radius: 5px;
+  text-align: center;
+
+  &:hover {
+    box-shadow: 0 8px 16px 0 $light-gray;
+  }
+}
+
+.text-name {
+  color: $primary-dark;
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+
+.text-location {
+  color: $primary;
+  font-weight: 100;
+}
+
+@media (max-width: $desktop) {
+  .user-list {
+    flex-wrap: wrap;
+  }
+}
+
+</style>
