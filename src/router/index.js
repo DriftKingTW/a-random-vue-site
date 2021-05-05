aa24f52290dca945f39150d27d49fc29184e82f3
DriftKingTW <driftkingtw@gmail.com>
2021-05-05 21:09:32 +0800

    Add todo and user page

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,17 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/user",
+    name: "User",
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (user.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import(/* webpackChunkName: "user" */ "../views/User.vue"),
+  },
+  {
+    path: "/todo",
+    name: "Todo",
+    component: () => import(/* webpackChunkName: "todo" */ "../views/Todo.vue"),
   },
 ];
 
