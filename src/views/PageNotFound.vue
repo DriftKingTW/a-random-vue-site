11f1c0be0f0e19362bdd01f595aeb38770529f9c
DriftKingTW <driftkingtw@gmail.com>
2021-05-06 13:25:45 +0800

    Lint code

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,11 +1,11 @@
 <template>
-    <h1>404 Sorry! This page does not exist!</h1>
+  <h1>404 Sorry! This page does not exist!</h1>
 </template>
 
 <style lang="scss" scoped>
 @import "@/assets/master.scss";
 
 h1 {
-    text-align: center;
+  text-align: center;
 }
 </style>
